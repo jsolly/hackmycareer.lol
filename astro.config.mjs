fdff6cc550d23e3ad6170055c53e89a5c43de569
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-09 14:13:28 -0500

    Use www canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hackmycareer.lol",
+	site: "https://www.hackmycareer.lol",
 	integrations: [tailwind(), sitemap()],
 	output: "static",
 	adapter: vercelStatic(),
